d4b672f4054df20f2edeca5c2ab07f15883e113f
Sean Loughrey <s.a.loughrey@gmail.com>
2024-05-27 16:54:51 -0400

    adding missing interface

--- models/UserTypeORMDataMapper.ts
+++ models/UserTypeORMDataMapper.ts
@@ -1,14 +1,15 @@
 import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm"
-import { injectable, inject, interfaces } from "inversify";
+import { injectable, inject } from "inversify";
 import "reflect-metadata";
 import InvTypes from "../types/inversifyTypes"
 import { iLogger } from "../interaces/iLogger"
+import iUser from "../interaces/iUser";
 
 
 
 @injectable()
 @Entity("users") // specify table name, otherwise it will use the class name as the table name
-export default class User {
+export default class User implements iUser {
     @PrimaryGeneratedColumn()
     id: number
 
